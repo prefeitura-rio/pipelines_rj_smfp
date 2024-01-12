fd4a5b240729ab4ee450624b56d3e31d1635957f
d116626 <d116626@gmail.com>
2024-01-12 11:28:43 -0300

    fix: missing comma

--- queries/models/recursos_humanos_ergon/ficha_financeira.sql
+++ queries/models/recursos_humanos_ergon/ficha_financeira.sql
@@ -12,19 +12,19 @@
 }}
 
 SELECT
-    SAFE_CAST(DATE(mes_ano_folha) AS DATE) AS data_folha,
-    SAFE_CAST(REGEXP_REPLACE(TRIM(num_folha), r'\.0$', '') AS INT64) AS numero_folha,
-    SAFE_CAST(REGEXP_REPLACE(TRIM(numfunc), r'\.0$', '') AS STRING) AS id_funcionario,
-    SAFE_CAST(REGEXP_REPLACE(TRIM(numvinc), r'\.0$', '') AS STRING) AS id_vinculo,
-    SAFE_CAST(REGEXP_REPLACE(TRIM(numpens), r'\.0$', '') AS INT64) AS numero_pensionista,
-    SAFE_CAST(TRIM(mes_ano_direito) AS STRING) AS data_pagamento,
+    SAFE_CAST(DATE(mes_ano_folha) AS DATE) AS mes_ano_folha,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(num_folha), r'\.0$', '') AS INT64) AS num_folha,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(numfunc), r'\.0$', '') AS STRING) AS numfunc,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(numvinc), r'\.0$', '') AS STRING) AS numvinc,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(numpens), r'\.0$', '') AS INT64) AS numpens,
+    SAFE_CAST(TRIM(mes_ano_direito) AS STRING) AS mes_ano_direito,
     SAFE_CAST(TRIM(rubrica) AS STRING) AS rubrica,
     SAFE_CAST(TRIM(tipo_rubrica) AS STRING) AS tipo_rubrica,
-    SAFE_CAST(TRIM(desc_vant) AS STRING) AS desconto,
-    SAFE_CAST(TRIM(complemento) AS STRING) AS complemento_rubrica,
-    SAFE_CAST(REGEXP_REPLACE(valor, r',', '.') AS FLOAT64) AS valor_rubrica,
+    SAFE_CAST(TRIM(desc_vant) AS STRING) AS desc_vant,
+    SAFE_CAST(TRIM(complemento) AS STRING) AS complemento,
+    SAFE_CAST(REGEXP_REPLACE(valor, r',', '.') AS FLOAT64) AS valor,
     SAFE_CAST(REGEXP_REPLACE(correcao, r',', '.') AS FLOAT64) AS correcao,
-    SAFE_CAST(REGEXP_REPLACE(TRIM(emp_codigo), r'\.0$', '') AS STRING) AS id_empresa,
+    SAFE_CAST(REGEXP_REPLACE(TRIM(emp_codigo), r'\.0$', '') AS STRING) AS emp_codigo,
     SAFE_CAST(data_particao AS DATE) data_particao,
 FROM rj-smfp.recursos_humanos_ergon_staging.ficha_financeira AS t
 WHERE
