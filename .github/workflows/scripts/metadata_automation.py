c22b91699696318b848041972a1c730548afc326
d116626 <d116626@gmail.com>
2024-02-27 04:00:36 -0300

    feat: add metadata automation

---
+++ .github/workflows/scripts/metadata_automation.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from prefeitura_rio.actions.fetch_metadata import fetch_metadata_and_save
+
+# from prefeitura_rio.actions.metadata_to_dbt_schema import metadata_to_dbt_schema
+
+
+if __name__ == "__main__":
+    fetch_metadata_and_save()
+    # metadata_to_dbt_schema()
