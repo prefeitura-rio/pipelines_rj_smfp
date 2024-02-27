6f053c143774f028ddb89577a9724dc342785b54
d116626 <d116626@gmail.com>
2024-02-27 09:49:38 -0300

    feat: add option to pass a list of dataset_ids to update metadata

--- .github/workflows/scripts/metadata_automation.py
+++ .github/workflows/scripts/metadata_automation.py
@@ -3,5 +3,6 @@ from prefeitura_rio.actions.fetch_metadata import fetch_metadata_and_save
 from prefeitura_rio.actions.metadata_to_dbt_schema import metadata_to_dbt_schema
 
 if __name__ == "__main__":
-    fetch_metadata_and_save()
-    metadata_to_dbt_schema()
+    dataset_ids = ["recursos_humanos_ergon_comlurb"]
+    fetch_metadata_and_save(dataset_ids=dataset_ids)
+    metadata_to_dbt_schema(dataset_ids=dataset_ids)
