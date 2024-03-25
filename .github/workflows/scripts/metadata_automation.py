7e174ae636c7defd310640367945484f7a5b00af
d116626 <d116626@gmail.com>
2024-03-25 13:15:39 -0300

    chore: add sigma queries and schema

--- .github/workflows/scripts/metadata_automation.py
+++ .github/workflows/scripts/metadata_automation.py
@@ -3,6 +3,6 @@ from prefeitura_rio.actions.fetch_metadata import fetch_metadata_and_save
 from prefeitura_rio.actions.metadata_to_dbt_schema import metadata_to_dbt_schema
 
 if __name__ == "__main__":
-    dataset_ids = ["recursos_humanos_ergon_comlurb"]
-    fetch_metadata_and_save(dataset_ids=dataset_ids)
+    dataset_ids = ["compras_materiais_servicos_sigma"]
+    fetch_metadata_and_save()
     metadata_to_dbt_schema(dataset_ids=dataset_ids)
