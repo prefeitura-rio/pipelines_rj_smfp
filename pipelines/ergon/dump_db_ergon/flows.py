f78d7df63e625f8ff342b423f9ab336cb2333050
d116626 <d116626@gmail.com>
2024-10-23 10:46:19 -0300

    chore: change from kubernetsRun to DockerRun

--- pipelines/ergon/dump_db_ergon/flows.py
+++ pipelines/ergon/dump_db_ergon/flows.py
@@ -24,7 +24,7 @@ dump_sql_ergon_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 dump_sql_ergon_flow.run_config = DockerRun(
     image=constants.DOCKER_IMAGE.value,
     labels=[
-        constants.RJ_SMFP_AGENT_LABEL.value,
+        constants.RJ_LOCAL_IPLAN_AGENT_LABEL.value,
     ],
 )
 
