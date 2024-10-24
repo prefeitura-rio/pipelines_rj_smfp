50ccd5e02a0605b19d77f8223190703317318954
Bruno Almeida <brunodepauloalmeida@gmail.com>
2024-10-24 13:24:57 -0300

    fix: queries with new column names and types

--- queries/models/planejamento_gestao_acordo_resultados/meta_desdobrada.sql
+++ queries/models/planejamento_gestao_acordo_resultados/meta_desdobrada.sql
@@ -13,13 +13,13 @@ SAFE_CAST(valor_de_referencia AS STRING) as valor_referencia,
 SAFE_CAST(data_de_referencia AS STRING) as data_referencia,
 SAFE_CAST(meta AS STRING) as meta,
 SAFE_CAST(relacionada_a_meta_estrategica AS STRING) as relacao_estrategia,
-SAFE_CAST(indicacao_para_auditoria_pela_cgm AS STRING) as indicacao_auditoria,
+SAFE_CAST(unidade_de_medida AS STRING) as unidade_de_medida,
 SAFE_CAST(gerencia AS STRING) as gerencia,
 SAFE_CAST(chave_meta_ar AS STRING) as chave_meta,
 SAFE_CAST(codigo_metaar AS STRING) as codigo_meta_ar,
 SAFE_CAST(SAFE_CAST(ref_resultado AS FLOAT64) AS INT64) as referencia_resultado,
 SAFE_CAST(resultado AS STRING) as resultado,
-SAFE_CAST(SAFE_CAST(ref_chance_meta_desdobrada AS FLOAT64) AS INT64) as referencia_chance_meta_desdobrada,
+SAFE_CAST(SAFE_CAST(ref_chance_meta_desdobrada AS FLOAT64) AS INT64) as referencia_chance_meta_desdobrada, 
 SAFE_CAST(SAFE_CAST(chance_meta_desdobrada AS FLOAT64) AS INT64) as chance_meta_desdobrada,
 SAFE_CAST(SAFE_CAST(ref_resultado2_performance AS FLOAT64) AS INT64) as referencia_resultado_orgao_2,
 SAFE_CAST(resultado2_performance AS STRING) as resultado_orgao_2,
@@ -34,20 +34,20 @@ SAFE_CAST(SAFE_CAST(cor_chance_meta AS FLOAT64) AS INT64) as chance_meta_orgao_1
 SAFE_CAST(SAFE_CAST(cor_chance_meta_orgao_2 AS FLOAT64) AS INT64) as chance_meta_orgao_2,
 CASE
     WHEN resultado_performance_prefeito = "TRUE" THEN TRUE
-    WHEN resultado_performance_prefeito = "FALSE" THEN FALSE
+    WHEN resultado_performance_prefeito = "FALSE" THEN FALSE 
     WHEN resultado_performance_prefeito = "VERDADEIRO" THEN TRUE
     WHEN resultado_performance_prefeito = "FALSO" THEN FALSE
     WHEN resultado_performance_prefeito = "Sim" THEN TRUE
     WHEN resultado_performance_prefeito = "Não" THEN FALSE
-    ELSE NULL
+    ELSE NULL         
 END as indicador_resultado_peformance_orgao_1,
 CASE
     WHEN resultado_performance2_prefeito = "TRUE" THEN TRUE
-    WHEN resultado_performance2_prefeito = "FALSE" THEN FALSE
+    WHEN resultado_performance2_prefeito = "FALSE" THEN FALSE 
     WHEN resultado_performance2_prefeito = "VERDADEIRO" THEN TRUE
     WHEN resultado_performance2_prefeito = "FALSO" THEN FALSE
     WHEN resultado_performance2_prefeito = "Sim" THEN TRUE
     WHEN resultado_performance2_prefeito = "Não" THEN FALSE
-    ELSE NULL
+    ELSE NULL         
 END as indicador_resultado_peformance_orgao_2
 FROM `rj-smfp.planejamento_gestao_acordo_resultados_staging.meta_desdobrada`
