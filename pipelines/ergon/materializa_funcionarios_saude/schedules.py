df84dac26d58a44a549673f067ea774d0db0e008
d116626 <d116626@gmail.com>
2024-11-05 12:41:33 -0300

    feat: add table funcionario, remove duplicity from provimento

--- pipelines/ergon/materializa_funcionarios_saude/schedules.py
+++ pipelines/ergon/materializa_funcionarios_saude/schedules.py
@@ -12,18 +12,23 @@ from prefeitura_rio.pipelines_utils.io import untuple_clocks as untuple
 
 from pipelines.constants import constants
 
+tables = [{"table_id": "funcionarios_ativos"}, {"table_id": "funcionarios"}]
+
+
 smfp_funcionarios_saude_clocks = [
     IntervalClock(
         interval=timedelta(days=1),
-        start_date=datetime(2021, 11, 23, 1, 0, tzinfo=pytz.timezone("America/Sao_Paulo")),
+        start_date=datetime(2021, 11, 23, 1, 0, tzinfo=pytz.timezone("America/Sao_Paulo"))
+        + timedelta(minutes=5 * count),
         labels=[
             constants.RJ_SMFP_AGENT_LABEL.value,
         ],
         parameter_defaults={
             "dataset_id": "recursos_humanos_ergon_saude",
-            "table_id": "funcionarios_ativos",
+            "table_id": parameter["table_id"],
         },
     )
+    for count, parameter in enumerate(tables)
 ]
 smfp_funcionarios_saude_daily_update_schedule = Schedule(
     clocks=untuple(smfp_funcionarios_saude_clocks)
