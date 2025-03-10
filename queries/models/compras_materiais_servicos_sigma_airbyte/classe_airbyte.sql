7d177fb50ec296afb9107399b0f2246e4ebab45e
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2025-03-10 15:02:58 -0300

    fix: Update docs

--- queries/models/compras_materiais_servicos_sigma_airbyte/classe_airbyte.sql
+++ queries/models/compras_materiais_servicos_sigma_airbyte/classe_airbyte.sql
@@ -1,7 +1,7 @@
 SELECT
-    SAFE_CAST(cd_classe AS STRING) AS id_classe,
-    SAFE_CAST(cd_grupo AS STRING) AS id_grupo,
+    SAFE_CAST(cd_classe AS STRING) AS codigo_classe,
+    SAFE_CAST(cd_grupo AS STRING) AS codigo_grupo,
     SAFE_CAST(ds_classe AS STRING) AS descricao_classe,
-    SAFE_CAST(st_status AS STRING) AS id_status,
+    SAFE_CAST(st_status AS STRING) AS status
 
 FROM `rj-smfp.compras_materiais_servicos_sigma_staging.VW_CLASSE` AS t
--- queries/models/compras_materiais_servicos_sigma_airbyte/material_referencia_airbyte.sql
+++ queries/models/compras_materiais_servicos_sigma_airbyte/material_referencia_airbyte.sql
@@ -1,13 +1,13 @@
 SELECT
-    SAFE_CAST(cd_classe AS STRING) AS id_classe,
-    SAFE_CAST(cd_grupo AS STRING) AS id_grupo,
-    SAFE_CAST(cd_material AS STRING) AS id_material,
-    SAFE_CAST(cd_referencia AS STRING) AS id_referencia,
-    SAFE_CAST(cd_subclasse AS STRING) AS id_subclasse,
+    SAFE_CAST(cd_classe AS STRING) AS codigo_classe,
+    SAFE_CAST(cd_grupo AS STRING) AS codigo_grupo,
+    SAFE_CAST(cd_material AS STRING) AS codigo_material,
+    SAFE_CAST(cd_referencia AS STRING) AS codigo_referencia,
+    SAFE_CAST(cd_subclasse AS STRING) AS codigo_subclasse,
     SAFE_CAST(ds_referencia AS STRING) AS descricao_referencia,
     SAFE_CAST(dv1 AS STRING) AS digito_verificador_1,
     SAFE_CAST(dv2 AS STRING) AS digito_verificador_2,
     SAFE_CAST(sequencial AS STRING) AS sequencial_material,
-    SAFE_CAST(st_status AS STRING) AS status,
+    SAFE_CAST(st_status AS STRING) AS status
 
 FROM `rj-smfp.compras_materiais_servicos_sigma_staging.VW_MATERIAL_REFERENCIA` AS t
