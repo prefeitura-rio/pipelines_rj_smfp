2820e44464182a4272efdc9b1193dbbf83636978
PedroSiqueira1 <pedro.siqueira@poli.ufrj.br>
2025-03-17 16:22:42 -0300

    fix: Name of the flow in init

--- pipelines/sigma/__init__.py
+++ pipelines/sigma/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from pipelines.sigma.dump_db_compras_materiais.flows import *  # noqa
+from pipelines.sigma.materialize_compras_materiais.flows import *  # noqa
 from pipelines.sigma.dump_db_sancao_fornecedor.flows import *  # noqa
