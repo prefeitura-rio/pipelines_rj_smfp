2959be96697ae6d40f7c1dad1d9f142cbd79dac9
d116626 <d116626@gmail.com>
2025-04-24 17:39:10 -0300

    chore: add brutos_ergon

--- pipelines/ergon/materializa_brutos_ergon/flows.py
+++ pipelines/ergon/materializa_brutos_ergon/flows.py
@@ -21,24 +21,24 @@ from pipelines.ergon.materializa_funcionarios_saude.schedules import (
     smfp_funcionarios_saude_daily_update_schedule,
 )
 
-run_dbt_smfp_funcionarios_saude = deepcopy(templates__run_dbt_model__flow)
-run_dbt_smfp_funcionarios_saude.name = "SMFP: brutos_ergon - Materializar tabelas"
-run_dbt_smfp_funcionarios_saude.state_handlers = [
+run_dbt_brutos_ergon = deepcopy(templates__run_dbt_model__flow)
+run_dbt_brutos_ergon.name = "SMFP: brutos_ergon - Materializar tabelas"
+run_dbt_brutos_ergon.state_handlers = [
     handler_inject_bd_credentials,
     handler_initialize_sentry,
 ]
-run_dbt_smfp_funcionarios_saude.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-run_dbt_smfp_funcionarios_saude.run_config = KubernetesRun(
+run_dbt_brutos_ergon.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+run_dbt_brutos_ergon.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value, labels=[constants.RJ_SMFP_AGENT_LABEL.value]
 )
 
-smfp_funcionarios_saude_default_parameters = {
+smfp_brutos_ergon_default_parameters = {
     "dataset_id": "brutos_ergon",
     "table_id": "cargo",
 }
-run_dbt_smfp_funcionarios_saude = set_default_parameters(
-    run_dbt_smfp_funcionarios_saude,
-    default_parameters=smfp_funcionarios_saude_default_parameters,
+run_dbt_brutos_ergon = set_default_parameters(
+    run_dbt_brutos_ergon,
+    default_parameters=smfp_brutos_ergon_default_parameters,
 )
 
-run_dbt_smfp_funcionarios_saude.schedule = smfp_funcionarios_saude_daily_update_schedule
+run_dbt_brutos_ergon.schedule = smfp_funcionarios_saude_daily_update_schedule
